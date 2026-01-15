a5e1099631f870ca1fafea1e1d7a54e7b8486ec8
benjaminhuang <benjaminhuanghuang@gmail.com>
2026-01-15 12:58:51 -0800

    update

--- _Courses/Fullstack Typescript with TailwindCSS and tRPC/easybooking/services/backend/src/trpc/appRouter.test.ts
+++ _Courses/Fullstack Typescript with TailwindCSS and tRPC/easybooking/services/backend/src/trpc/appRouter.test.ts
@@ -1,12 +1,39 @@
 import { describe, expect, it } from 'vitest';
 
 import appRouter from './appRouter';
+import useTestDatabase from '../test-helpers/useTestDatabase';
 
 describe('appRouter', () => {
+  useTestDatabase();
+
   it('should ping/pong', async () => {
     const caller = appRouter.createCaller({});
 
     const r = await caller.ping();
     expect(r).toBe('pong');
+  });
+
+  it('should return the existing service types', async () => {
+    const caller = appRouter.createCaller({});
+
+    const serviceTypes = await caller.getServiceTypes();
+    expect(serviceTypes).toMatchObject([]);
+  });
+
+  it('should create service type and return the ID', async () => {
+    const caller = appRouter.createCaller({});
+
+    const createdServiceType = await caller.createServiceType({
+      name: 'Test Service Type',
+      description: 'Test Service Type Description',
+    });
+
+    // We don't really care about the actually ID, but if it's not 1,
+    // something, somewhere is wrong so this assertion does provide value.
+    expect(createdServiceType).toMatchObject({
+      id: 1,
+      name: 'Test Service Type',
+      description: 'Test Service Type Description',
+    });
   });
 });
--- _Courses/Fullstack Typescript with TailwindCSS and tRPC/easybooking/services/backend/src/trpc/appRouter.ts
+++ _Courses/Fullstack Typescript with TailwindCSS and tRPC/easybooking/services/backend/src/trpc/appRouter.ts
@@ -4,13 +4,29 @@ import { serviceType } from '@easybooking/schema/dist/models/public/serviceType'
 
 const getServiceTypesOutput = z.array(serviceType);
 
+const createServiceTypeInput = z.object({
+  name: z.string().min(1).max(255),
+  description: z.string().max(1000).optional(),
+});
+
 const appRouter = router({
   ping: publicProcedure.output(String).query(() => 'pong'),
+
   getServiceTypes: publicProcedure
     .output(getServiceTypesOutput)
     .query(async ({ ctx }) => {
       const serviceTypes = await ctx.trx('service_type').select('*');
       return serviceTypes;
+    }),
+
+  createServiceType: publicProcedure
+    .input(createServiceTypeInput)
+    .mutation(async ({ ctx, input }) => {
+      const [newServiceType] = await ctx
+        .trx('service_type')
+        .insert(input)
+        .returning('*');
+      return newServiceType;
     }),
 });
 
